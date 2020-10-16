3b6380b5e08f988381f9e85a7ff179936a2cb742
Bernardo Salazar <bersalazar@gmail.com>
2020-10-16 09:25:00 +0200

    Improve output formatting

--- output.py
+++ output.py
@@ -7,26 +7,34 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.enums import TA_JUSTIFY
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--output', '-o', help='output file name', default='./quotes.pdf')
 arg_parser.add_argument('--source', '-s', help='path to the source db', default='./db.json')
 args = arg_parser.parse_args()
 
-document = []
 if not os.path.isfile(args.source):
     logger.error(f'database does not exist at {args.source}')
     exit() 
 
+author_style = ParagraphStyle('source')
+author_style.fontSize = 7
+author_style.textColor = 'grey'
+text_style = ParagraphStyle('text')
+text_style.alignment=TA_JUSTIFY
+text_style.fontSize = 10
 previous_book = ''
+document = [Spacer(1,1)]
 for quote in TinyDB(args.source):
-    if previous_book != quote['book']:
-        document.append(Spacer(1, 10))
-        document.append(Paragraph(f"{quote['book']}"))
-        document.append(Paragraph(f"by {quote['author']}"))
-    document.append(Paragraph(f"- {quote['text']} ({quote.doc_id})"))
-    document.append(Spacer(1, 20))
+    #if previous_book != quote['book']:
+    #    document.append(Paragraph(f"{quote['book']}"))
+    #    document.append(Paragraph(f"by {quote['author']}"))
+    document.append(Paragraph(f"{quote['text']}", text_style))
+    document.append(Paragraph(f"{quote['book']} by {quote['author']} [{quote.doc_id}]", author_style))
+    document.append(Spacer(1, 10))
     previous_book = quote['book']
 
-SimpleDocTemplate(args.output, pagesize=letter).build(document)
+doc = SimpleDocTemplate(args.output, pagesize=letter)
+doc.build(document)
 logger.info(f'Successfully created {args.output}')
