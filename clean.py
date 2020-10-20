9eae8708817382f5a3040f8d900ec275a1ee87c6
Bernardo Salazar <bersalazar@gmail.com>
2020-10-20 09:07:38 +0200

    Clean quotes by ID and add config file

--- clean.py
+++ clean.py
@@ -2,21 +2,29 @@ import os
 import argparse
 from model import Quote, logger
 from tinydb import TinyDB, Query, where
+from config import config
 
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('--source', '-s', help='path to the source db', default='./db.json')
+arg_parser.add_argument('--key', '-k', help='clean a single quote by its ID')
 args = arg_parser.parse_args()
 
-db_path = './db.json'
+db_path = config['database_path']
 db = TinyDB(db_path)
 
-previous_quote = ''
-duplicates = []
-for quote in TinyDB(args.source):
-    if quote['text'].startswith(previous_quote[:25]):
-        duplicates.append(quote.doc_id-1)
-    previous_quote = quote['text']
+if args.key:
+    try:
+        db.remove(doc_ids=[int(args.key)])
+        logger.info(f'Removed quote with ID {args.key}')
+    except KeyError as ex:
+        logger.error('The specified key was not found', ex)
+else:
+    previous_quote = ''
+    duplicates = []
+    for quote in db:
+        if quote['text'].startswith(previous_quote[:25]):
+            duplicates.append(quote.doc_id-1)
+        previous_quote = quote['text']
 
-duplicates.pop(0)
-db.remove(doc_ids=duplicates)
-logger.info(f'Removed {len(duplicates)} duplicate records')
+    duplicates.pop(0)
+    db.remove(doc_ids=duplicates)
+    logger.info(f'Removed {len(duplicates)} duplicate records')
---
+++ config.py
@@ -0,0 +1,3 @@
+config = {
+    'database_path': './db.json'
+}
